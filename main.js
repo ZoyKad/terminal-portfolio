af8d588bcaf67d88cad9e5f68b5a4987aa60a517
ZoyKad <maxcaron@outlook.com>
2023-08-03 11:48:45 +0200

    fix(animation): awaiting fixed

--- main.js
+++ main.js
@@ -52,32 +52,32 @@ const contact = async () => {
      Discord : https://discordapp.com/users/675031196693954606 \n \
      Roblox : https://www.roblox.com/users/1097155733/profile \n \
      Github : https://github.com/ZoyKad")
-}
+};
 
 const delay = time => new Promise(r => setTimeout(r, time));
 
 // Animation
 
 const animate = async () => {
-    term.clear().set_prompt("")
-    await delay(1000)
-    term.clear()
+    term.clear().set_prompt("");
+    await delay(1000);
+    term.clear();
     
-    await term.echo("Knock, knock...", {typing: true, delay: 190})
-    await delay(1500)
+    await term.echo("Knock, knock...", {typing: true, delay: 190});
+    await delay(1500);
     
-    await term.echo("Wonder who I am ?", {typing: true, delay:130})
-    await delay(2000)
+    await term.echo("Wonder who I am ?", {typing: true, delay:130});
+    await delay(2000);
     
-    await term.echo("You can call me zoy", {typing: true, delay:150})
-    await delay(500)
+    await term.echo("You can call me zoy", {typing: true, delay:150});
+    await delay(500);
     
-    await term.echo("Type 'help' to get more info", {typing: true, delay:150})
-    await delay(500)
+    await term.echo("Type 'help' to get more info", {typing: true, delay:150});
+    await delay(500);
 
-    animating = false
-}
+    term.set_prompt(COMMAND_PREFIX);
 
-animate()
+    animating = false;
+};
 
-term.set_prompt(COMMAND_PREFIX)
+animate();
