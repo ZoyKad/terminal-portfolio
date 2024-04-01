bce69895641976c624427254491fb2b0e0f7f8aa
ZoyKad <77884733+ZoyKad@users.noreply.github.com>
2024-04-01 16:01:16 +0200

    Fix animation stoping

---
+++ constants.js
@@ -0,0 +1,29 @@
+export const COMMAND_PREFIX = "guest@zoykad-portofolio $ "
+
+export const HELP_TEXT = `List of all commands :
+ help\t\t\tThis menu
+ echo [text]\tprints the given text
+ carrd\t\t\tOpen my carrd
+ contact\t\tSee ways to contact me and my profiles
+ zoyart\t\t\t???
+ clear\t\t\tClear the terminal`
+
+export const CONTACT_TEXT = `Here are all my profiles and way to contact me : \n \
+Discord :\thttps://discordapp.com/users/675031196693954606 \n \
+Roblox :\thttps://www.roblox.com/users/1097155733/profile \n \
+Github :\thttps://github.com/ZoyKad \n \
+Email :\tmaxime@zoykad.works`
+
+export const ZOYKAD_ART = `
+$$$$$$$$\\                     $$\\   $$\\                 $$\\ 
+\\____$$  |                    $$ | $$  |                $$ |
+    $$  /  $$$$$$\\  $$\\   $$\\ $$ |$$  /  $$$$$$\\   $$$$$$$ |
+   $$  /  $$  __$$\\ $$ |  $$ |$$$$$  /   \\____$$\\ $$  __$$ |
+  $$  /   $$ /  $$ |$$ |  $$ |$$  $$<    $$$$$$$ |$$ /  $$ |
+ $$  /    $$ |  $$ |$$ |  $$ |$$ |\\$$\\  $$  __$$ |$$ |  $$ |
+$$$$$$$$\\ \\$$$$$$  |\\$$$$$$$ |$$ | \\$$\\ \\$$$$$$$ |\\$$$$$$$ |
+\\________| \\______/  \\____$$ |\\__|  \\__| \\_______| \\_______|
+                    $$\\   $$ |                              
+                    \\$$$$$$  |                              
+                     \\______/                               
+`
--- main.js
+++ main.js
@@ -1,45 +1,22 @@
+import * as constants from "./constants.js";
+
 let animating;
-
-const COMMAND_PREFIX = "guest@zoykad-portofolio $ "
-
-const HELP_TEXT = `List of all commands :
- help\t\t\tThis menu
- echo [text]\tprints the given text
- contact\t\tSee ways to contact me and my profiles
- zoyart\t\t\t???`
-
-const CONTACT_TEXT = `Here are all my profiles and way to contact me : \n \
-Discord : https://discordapp.com/users/675031196693954606 \n \
-Roblox : https://www.roblox.com/users/1097155733/profile \n \
-Github : https://github.com/ZoyKad`
-
-const ZOYKAD_ART = `
-$$$$$$$$\\                     $$\\   $$\\                 $$\\ 
-\\____$$  |                    $$ | $$  |                $$ |
-    $$  /  $$$$$$\\  $$\\   $$\\ $$ |$$  /  $$$$$$\\   $$$$$$$ |
-   $$  /  $$  __$$\\ $$ |  $$ |$$$$$  /   \\____$$\\ $$  __$$ |
-  $$  /   $$ /  $$ |$$ |  $$ |$$  $$<    $$$$$$$ |$$ /  $$ |
- $$  /    $$ |  $$ |$$ |  $$ |$$ |\\$$\\  $$  __$$ |$$ |  $$ |
-$$$$$$$$\\ \\$$$$$$  |\\$$$$$$$ |$$ | \\$$\\ \\$$$$$$$ |\\$$$$$$$ |
-\\________| \\______/  \\____$$ |\\__|  \\__| \\_______| \\_______|
-                    $$\\   $$ |                              
-                    \\$$$$$$  |                              
-                     \\______/                               
-`
-
 
 const term = $('body').terminal({
     contact: function() {
-        term.echo(CONTACT_TEXT)
+        term.echo(constants.CONTACT_TEXT)
     },
     help: function() {
-        term.echo(HELP_TEXT)
+        term.echo(constants.HELP_TEXT)
     },
     echo: function(text) {
         term.echo(text !== null ? text: "")
     },
     zoyart: function() {
-        term.echo(ZOYKAD_ART)
+        term.echo(constants.ZOYKAD_ART)
+    },
+    carrd: function() {
+        window.open("https://zoykad.carrd.co/")
     }
 }, {
     greetings: false,
@@ -47,35 +24,29 @@ const term = $('body').terminal({
     keydown: () => animating ? false : undefined
 });
 
-const contact = async () => {
-    await term.echo("Here are all my profiles and way to contact me : \n \
-     Discord : https://discordapp.com/users/675031196693954606 \n \
-     Roblox : https://www.roblox.com/users/1097155733/profile \n \
-     Github : https://github.com/ZoyKad")
-};
 
 const delay = time => new Promise(r => setTimeout(r, time));
 
 // Animation
-
-const animate = async () => {
+async function animate() {
     term.clear().set_prompt("");
     await delay(1000);
     term.clear();
     
-    await term.echo("Knock, knock...", {typing: true, delay: 190});
+    await term.echo("");
+    await term.echo("Knock, knock...", {typing: true, delay:60});
     await delay(1500);
     
-    await term.echo("Wonder who I am ?", {typing: true, delay:130});
+    await term.echo("Wonder who I am ?", {typing: true, delay:70});
     await delay(2000);
     
-    await term.echo("You can call me zoy", {typing: true, delay:150});
+    await term.echo("You can call me zoy", {typing: true, delay:70});
     await delay(500);
     
-    await term.echo("Type 'help' to get more info", {typing: true, delay:150});
+    await term.echo("Type \"help\" for more information", {typing: true, delay:70});
     await delay(500);
 
-    term.set_prompt(COMMAND_PREFIX);
+    term.set_prompt(constants.COMMAND_PREFIX);
 
     animating = false;
 };
